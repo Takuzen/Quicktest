22115fc441e83410908865d76ecd9889fd28a7af
Hideo Yasumoto <hideo54@hideo54.com>
2021-08-28 14:51:14 +0900

    決済終了でカート削除

--- pages/success.js
+++ pages/success.js
@@ -1,7 +1,17 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useCookies } from "react-cookie";
 
 export default function Home() {
+  const router = useRouter();
+  const [,, removeCookie] = useCookies([
+    "item_sample",
+  ]);
+  const sessionId = router.query.session_id;
+  if (sessionId) {
+    removeCookie("item_sample");
+  }
   return (
     <div className={styles.successMsgDiv}>
       <h1 className={styles.successText}>予約注文が完了しました！</h1>
