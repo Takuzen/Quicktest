da7c3e9dcb4cf188fa47d35be6e8d8488230fcfd
takuzen <takuzen0430@gmail.com>
2021-08-29 17:00:53 +0900

    homepage basically done, add-to-cart-btn is left

--- pages/antigen.js
+++ pages/antigen.js
@@ -3,26 +3,29 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import { useCookies } from "react-cookie";
 
-const range = (n) => Array.from(Array(n), (_, k) => k+1);
+const range = (n) => Array.from(Array(n), (_, k) => k + 1);
 
 export default function Home() {
   const [cookies, setCookie] = useCookies([]);
   const updateCookie = (item, _count) => {
     const current = parseInt(cookies[`item_${item}`]);
     const count = parseInt(_count);
-    setCookie(
-      `item_${item}`,
-      current && current ? current + count : count
-    );
+    setCookie(`item_${item}`, current && current ? current + count : count);
   };
   const [count, setCount] = useState(1);
   const router = useRouter();
   return (
     <div className={styles.antigenPageContainer}>
-      <button onClick={() => {
-        updateCookie('sample', count);
-        router.push('/cart');
-      }}>カートに入れる</button>
+      <button
+        onClick={() => {
+          updateCookie("sample", count);
+          router.push("/cart");
+        }}
+      >
+        カートに入れる
+      </button>
+
+      {/*
       <select
         value={count}
         onChange={(e) => {
@@ -33,6 +36,7 @@ export default function Home() {
           <option key={`sample-${i}`}>{i}</option>
         ))}
       </select>
+      */}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,7 @@ import Link from "next/link";
 import { useCookies } from "react-cookie";
 
 export default function Home() {
-  const [cookies] = useCookies([
-    "item_sample",
-  ]);
+  const [cookies] = useCookies(["item_sample"]);
   return (
     <div className={styles.container}>
       <Head>
@@ -22,34 +20,102 @@ export default function Home() {
       <main className={styles.main}>
         <header className={styles.header}>
           <div className={styles.headerLeft}>
-            <h3>QuickTest</h3>
+            <Image
+              className={styles.textLogoContainer}
+              src="/logo-text.png"
+              alt="logo-text"
+              width={150}
+              height={40}
+            ></Image>
           </div>
           <div className={styles.headerRight}>
             <Link href="/cart">
               <a className={styles.card}>
-              <Image
-                src={cookies.item_sample > 0 ? "/cart-reddot.svg" : "/cart.svg"}
-                alt="cart.svg"
-                width={30}
-                height={30}
-              />
+                <Image
+                  src={
+                    cookies.item_sample > 0 ? "/cart-reddot.svg" : "/cart.svg"
+                  }
+                  alt="cart.svg"
+                  width={30}
+                  height={30}
+                />
               </a>
             </Link>
           </div>
         </header>
 
+        <section className={styles.campaignLine}>
+          <p>現在予約受付中！</p>
+        </section>
+
+        <section className={styles.productName}>
+          <p className={styles.PNFirstLine}>新型コロナウィルス</p>
+          <p className={styles.PNSecondLine}>抗原検査キット</p>
+        </section>
+
         <div>
           <Link href="/antigen">
             <a className={styles.card}>
               <Image
-                src="/cart.svg"
-                alt="cart.svg"
-                width={30}
-                height={30}
+                src="/icheck.png"
+                alt="icheck"
+                width={300}
+                height={150}
               ></Image>
             </a>
           </Link>
         </div>
+
+        <section className={styles.productFeatures}>
+          <p className={styles.whatAreQTFeatures}>クイックテストの特徴</p>
+
+          <div className={styles.featuresColumns}>
+            <Image
+              className={styles.feature}
+              src="/feature1.png"
+              alt="feature1"
+              width={75}
+              height={135}
+            ></Image>
+            <Image
+              className={styles.feature}
+              src="/feature2.png"
+              alt="feature2"
+              width={75}
+              height={135}
+            ></Image>
+            <Image
+              className={styles.feature}
+              src="/feature3.png"
+              alt="feature3"
+              width={75}
+              height={135}
+            ></Image>
+            <Image
+              className={styles.feature}
+              src="/feature4.png"
+              alt="feature4"
+              width={75}
+              height={135}
+            ></Image>
+          </div>
+
+          <Image
+            className={styles.weWillAnswer}
+            src="/wewillanswer.png"
+            alt="wewillanswer"
+            width={240}
+            height={45}
+          ></Image>
+
+          <Image
+            className={styles.weShip365}
+            src="/weship365.png"
+            alt="weship365"
+            width={160}
+            height={45}
+          ></Image>
+        </section>
       </main>
 
       <footer className={styles.footer}>
