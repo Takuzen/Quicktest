1f33658de5cb808a36c4941025b80a98eea9e925
takuzen <takuzen0430@gmail.com>
2021-09-05 01:19:48 +0900

    commented out campaignline and add meta description and html lang

--- pages/index.js
+++ pages/index.js
@@ -17,128 +17,134 @@ export default function Home() {
   const router = useRouter();
   return (
     <div className={styles.container}>
-      <Head>
-        <title>【公式】QuickTest</title>
-        <meta
-          name="QuickTest"
-          content="簡易コロナ検査キットの「クイックテスト」"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <html lang="ja">
+        <Head>
+          <title>【公式】QuickTest</title>
+          <meta
+            name="description"
+            content="新型コロナウィルスに対応した抗原検査キットの「クイックテスト」製品の紹介。子供でも自分でできるイラスト使用ガイド付き。最速10分で、変異株をも見逃さず、PCR検査に劣らない特異度の正確判定。陰性であった場合、無料で即日スマホからずっと見せられる、デジタル陰性証明書をお送りします。365日発送対応いたします。"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <header className={styles.header}>
-          <div className={styles.headerLeft}>
-            <Image
-              className={styles.textLogoContainer}
-              src="/logo.png"
-              alt="logo"
-              width={150}
-              height={40}
-            ></Image>
-          </div>
-          <div className={styles.headerRight}>
-            <Link href="/cart">
-              <a className={styles.card}>
-                <Image
-                  src={
-                    cookies.item_sample > 0 ? "/cart-reddot.svg" : "/cart.svg"
-                  }
-                  alt="cart.svg"
-                  width={30}
-                  height={30}
-                />
-              </a>
-            </Link>
-          </div>
-        </header>
+        <main className={styles.main}>
+          <header className={styles.header}>
+            <div className={styles.headerLeft}>
+              <Image
+                className={styles.textLogoContainer}
+                src="/logo.png"
+                alt="logo"
+                width={150}
+                height={40}
+              ></Image>
+            </div>
+            <div className={styles.headerRight}>
+              <Link href="/cart">
+                <a className={styles.card}>
+                  <Image
+                    src={
+                      cookies.item_sample > 0 ? "/cart-reddot.svg" : "/cart.svg"
+                    }
+                    alt="cart.svg"
+                    width={30}
+                    height={30}
+                  />
+                </a>
+              </Link>
+            </div>
+          </header>
+
+          {/* 
 
         <section className={styles.campaignLine}>
           <p>🔥 23区内は18時までのご注文で当日お届け 🔥</p>
         </section>
 
-        <section className={styles.productName}>
-          <p className={styles.PNFirstLine}>新型コロナウィルス</p>
-          <p className={styles.PNSecondLine}>抗原検査キット</p>
-        </section>
+        */}
 
-        <div>
-          <Link href="/antigen">
-            <a className={styles.card}>
+          <section className={styles.productName}>
+            <p className={styles.PNFirstLine}>新型コロナウィルス</p>
+            <p className={styles.PNSecondLine}>抗原検査キット</p>
+          </section>
+
+          <div>
+            <Link href="/antigen">
+              <a className={styles.card}>
+                <Image
+                  className={styles.productImg}
+                  src="/LionRun-antigen-box.png"
+                  alt="LionRun-antigen-box"
+                  width={350}
+                  height={180}
+                ></Image>
+              </a>
+            </Link>
+          </div>
+
+          <section className={styles.productFeatures}>
+            <p className={styles.whatAreQTFeatures}>クイックテストの特徴</p>
+
+            <div className={styles.featuresColumns}>
               <Image
-                className={styles.productImg}
-                src="/LionRun-antigen-box.png"
-                alt="LionRun-antigen-box"
-                width={350}
-                height={180}
+                className={styles.feature}
+                src="/feature1.png"
+                alt="feature1"
+                width={75}
+                height={135}
               ></Image>
-            </a>
-          </Link>
-        </div>
+              <Image
+                className={styles.feature}
+                src="/feature2.png"
+                alt="feature2"
+                width={75}
+                height={135}
+              ></Image>
+              <Image
+                className={styles.feature}
+                src="/feature3.png"
+                alt="feature3"
+                width={75}
+                height={135}
+              ></Image>
+              <Image
+                className={styles.feature}
+                src="/feature4.png"
+                alt="feature4"
+                width={75}
+                height={135}
+              ></Image>
+            </div>
 
-        <section className={styles.productFeatures}>
-          <p className={styles.whatAreQTFeatures}>クイックテストの特徴</p>
+            <div className={styles.otherTwoFeatures}>
+              <Image
+                className={styles.weWillAnswer}
+                src="/wewillanswer.png"
+                alt="wewillanswer"
+                width={240}
+                height={45}
+              ></Image>
 
-          <div className={styles.featuresColumns}>
-            <Image
-              className={styles.feature}
-              src="/feature1.png"
-              alt="feature1"
-              width={75}
-              height={135}
-            ></Image>
-            <Image
-              className={styles.feature}
-              src="/feature2.png"
-              alt="feature2"
-              width={75}
-              height={135}
-            ></Image>
-            <Image
-              className={styles.feature}
-              src="/feature3.png"
-              alt="feature3"
-              width={75}
-              height={135}
-            ></Image>
-            <Image
-              className={styles.feature}
-              src="/feature4.png"
-              alt="feature4"
-              width={75}
-              height={135}
-            ></Image>
-          </div>
+              <Image
+                className={styles.weShip365}
+                src="/weship365.png"
+                alt="weship365"
+                width={160}
+                height={45}
+              ></Image>
+            </div>
+          </section>
 
-          <div className={styles.otherTwoFeatures}>
-            <Image
-              className={styles.weWillAnswer}
-              src="/wewillanswer.png"
-              alt="wewillanswer"
-              width={240}
-              height={45}
-            ></Image>
-
-            <Image
-              className={styles.weShip365}
-              src="/weship365.png"
-              alt="weship365"
-              width={160}
-              height={45}
-            ></Image>
-          </div>
-        </section>
-
-        <button
-          className={styles.applyBtn}
-          onClick={() => {
-            updateCookie("sample", count);
-            router.push("/cart");
-          }}
-        >
-          今すぐ申し込む
-        </button>
-      </main>
+          <button
+            className={styles.applyBtn}
+            onClick={() => {
+              updateCookie("sample", count);
+              router.push("/cart");
+            }}
+          >
+            今すぐ申し込む
+          </button>
+        </main>
+      </html>
     </div>
   );
 }
